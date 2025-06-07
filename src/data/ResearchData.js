54116e82183800358e708f336f6a5f02bb54d1ec
Moreen Hettihewa <moreenmadhushani@gmail.com>
2025-06-07 10:23:20 +0530

    Data modifications

--- src/data/ResearchData.js
+++ src/data/ResearchData.js
@@ -2,7 +2,7 @@ import firstImage from "./../images/overview01_1.jpg";
 import secondImage from "./../images/overview02_1.jpg";
 import thirdImage from "./../images/overview03_1.png";
 import forthImage from "./../images/overview04_4.jpeg"
-import fifthImage from "./../images/overview05_1.png"
+import fifthImage from "./../images/overview05_4.jpg"
 const basePath = "/chanuka.io/#/overview";
 
 export const ResearchContent = [
